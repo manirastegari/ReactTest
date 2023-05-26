3d338a3d94f7a3a9eb5987499244ae8609dc3249
Mani Rastegari <mani.rastegari@gmail.com>
2023-05-26 17:40:01 -0400

    usingPropsAndMappingComponents

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,41 @@
 import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import Note from "./Note";
+import Card from "./Card";
+import contacts from "../contacts";
+
+function createCard(contact) {
+  return (
+    <Card
+      name={contact.name}
+      img={contact.imgURL}
+      tel={contact.phone}
+      email={contact.email}
+    />
+  );
+}
 
 function App() {
   return (
     <div>
-      <Header />
-      <Note />
-      <Footer />
+      <h1 className="heading">My Contacts</h1>
+      {contacts.map(createCard)}
+      {/* <Card
+        name={contacts[0].name}
+        img={contacts[0].imgURL}
+        tel={contacts[0].phone}
+        email={contacts[0].email}
+      />
+      <Card
+        name={contacts[1].name}
+        img={contacts[1].imgURL}
+        tel={contacts[1].phone}
+        email={contacts[1].email}
+      />
+      <Card
+        name={contacts[2].name}
+        img={contacts[2].imgURL}
+        tel={contacts[2].phone}
+        email={contacts[2].email}
+      /> */}
     </div>
   );
 }
